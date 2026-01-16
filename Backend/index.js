1cf7855d7cb31ebc1121d6a3270dc038d2a29781
Mohit Kumar <mohitkataria.tech@gmail.com>
2026-01-16 16:21:43 +0530

    added-cors-updation

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import postRouter from "./routes/post.routes.js";
-import loopRouter  from "./routes/loop.routes.js";
+import loopRouter from "./routes/loop.routes.js";
 import storyRouter from "./routes/story.routes.js";
 import messageRouter from "./routes/message.routes.js";
 import { app, server } from "./socket.js";
@@ -14,15 +14,15 @@ dotenv.config()
 
 const port = process.env.PORT
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://vibe-silk-one.vercel.app", credentials: true,  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], }));
 
 
 app.use(express.json());
 app.use(cookieParser())
 
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
-app.use("/api/post",postRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
+app.use("/api/post", postRouter)
 app.use("/api/loop", loopRouter)
 app.use("/api/story", storyRouter)
 app.use("/api/message", messageRouter)
@@ -32,7 +32,7 @@ app.use("/api/message", messageRouter)
 
 
 
-server.listen(port, ()=>{ 
+server.listen(port, () => {
     connectDb()
     console.log("server started")
 })
